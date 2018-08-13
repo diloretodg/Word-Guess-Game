8bd750702c7f0e70bf16392b04d5ea46123c8f6e
Diloretodg <diloretodg74122@gmail.com>
2018-08-12 21:08:38 -0400

    win lose functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,22 @@
 var wordDisplay = document.getElementById("word-display");
 var guessDisplay = document.getElementById("guess-display");
 var letterDisplay = document.getElementById("letter-display");
+var livesDisplay = document.getElementById("lives-display");
+var winDisplay = document.getElementById("win-display");
+var newGameBtn = document.getElementById("new-game-btn")
 var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y', 'z'];
 var wordSelection = ["panda","red panda","owl"];
-var currentChoiceIndex = Math.floor(Math.random()*wordSelection.length);
-var wordChoice = wordSelection[currentChoiceIndex];
-var currentWord = Array.from(wordChoice);
-var guessedLetters = [];
+var currentChoiceIndex;
+var wordChoice;
+var currentWord;
 var guessWord = [];
+var guessedLetters = [];
+var livesLeft = 7;
+var win = 0;
 
-startGame();
+newGameBtn.onclick = function() {
+    startGame();
+} 
 
 document.onkeydown = function(event) {
     makeGuess(event.key);
@@ -20,45 +27,106 @@ function makeGuess(key) {
         if (key === alphabet[i]) {
             if (guessedLetters.indexOf(key) === -1) {
                 guessedLetters.push(key);
-                updateGuess(key);
                 checkGuess(key);
             }   
         }
     }
 };
 
-function updateGuess() {
+function updateWinDisplay() {
+    winDisplay.textContent = ("Wins: " + win);
+}
+
+function updateLivesDisplay() {
+    livesDisplay.textContent = ("Lives Remaining: " + livesLeft);
+}
+
+function updateGuessDisplay () {
     guessDisplay.textContent = guessedLetters;
 }
 
 function pickWord() {
-    currentChoiceIndex;
+    currentChoiceIndex = Math.floor(Math.random()*wordSelection.length);
+    wordChoice = wordSelection[currentChoiceIndex];
+    currentWord = Array.from(wordChoice);;
     guessWordLength();
     setWordDisplay();
 }
 function guessWordLength() {
     for (i = 0; i < currentWord.length; i++) {
-        guessWord.push("_ ,");
+        if (currentWord[i] != " "){
+            guessWord.push("_ ,");
+        } else {
+            guessWord.push(" ")
+        }
      };
 }
 
 function setWordDisplay() {
     for (var i = 0; i < guessWord.length; i++) {
-        var newBlank = document.createElement("a");wordDisplay.appendChild(newBlank);
-        newBlank.setAttribute("id", "blank" + i);
-        newBlank.innerHTML = guessWord[i];
+        var newBlank = document.createElement("a");
+        newBlank.setAttribute("class", "px-1")
+        if (guessWord[i] === " ") {
+            newBlank.setAttribute("id", "blank-space")
+            newBlank.textContent = " ";
+            wordDisplay.appendChild(newBlank);
+        } else {
+            newBlank.setAttribute("id", "blank-" + i);
+            newBlank.textContent = guessWord[i];
+            wordDisplay.appendChild(newBlank);
+        }
     };
 }
 
 function startGame() {
+    wordDisplay.innerHTML = "";
+    guessWord = [];
+    livesLeft = 7;
     guessedLetters = [];
-    updateGuess();
     pickWord();
+    updateDisplay();
 }
 
 function checkGuess(letter) {
+    var rightGuess = false;
     for (var i = 0; i < currentWord.length; i++) {
-        if (letter === currentWord[i])
-            document.getElementById("blank" + i).textContent = currentWord[i];
+        if (letter === currentWord[i]) {
+            document.getElementById("blank-" + i).textContent = currentWord[i];
+            guessWord[i] = currentWord[i];
+            rightGuess = true;
+        }
     }
+    if (rightGuess === false){
+        livesLeft --;
+        updateDisplay();
+        checkLose();
+    } else {
+        updateDisplay();
+        checkWin();
+    }
+}
+
+function checkWin() {
+    var winner = true;
+    for (var i = 0; i < currentWord.length; i++) {
+        if (currentWord[i] != guessWord[i]) {
+            winner = false;
+        }
+    }
+    if (winner === true) {
+        alert(" you win!")
+    }
+}
+
+function checkLose() {
+    if (livesLeft <= 0) {
+        alert("You lose. The right word was " + wordChoice)
+    }
+
+}
+
+function updateDisplay() {
+    updateGuessDisplay();
+    updateLivesDisplay();
+    updateWinDisplay();
 }
